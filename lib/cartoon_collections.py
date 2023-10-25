cc67baedcfa0cc78a0523cc4ee6d06365a1c0c84
Joseph <josephylee72@gmail.com>
2023-10-25 15:02:55 -0700

    completed cartoon collections lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves_list):
+    [print(f"{dwarves_list.index(dwarf) + 1}. {dwarf}") for dwarf in dwarves_list]
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(pcalls_list):
+    return [f"{call.title()}!" for call in pcalls_list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(long_pcalls_list):
+    for call in long_pcalls_list:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(cheese_list):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+    for cheese in cheese_list:
+        if cheese in cheeses:
+            return cheese
+    return None
